a27a4c9ae854de863f498b95924329d09ab05560
UremSept <397055873@qq.com>
2016-05-15 17:08:42 +0800

    音乐播放

--- src/main/java/com/example/houjie/studyapk/studymedia/video/ActivityMediaInternetVideo.java
+++ src/main/java/com/example/houjie/studyapk/studymedia/video/ActivityMediaInternetVideo.java
@@ -10,27 +10,33 @@ import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.SeekBar;
 
 import com.example.houjie.studyapk.R;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 
-public class ActivityMediaInternetVideo extends AppCompatActivity implements MediaPlayer.OnPreparedListener{
+public class ActivityMediaInternetVideo extends AppCompatActivity implements MediaPlayer.OnPreparedListener
+,MediaPlayer.OnBufferingUpdateListener{
     private String path = "http://172.6.1.73:8080/AndroidWebServer/video1.mp4";
     private EditText mediaInternetVideoEdit;
     private SurfaceView surfaceView;
     private Button button;
     private MediaPlayer mediaPlay;
     private boolean aBoolean = false;
+    private SeekBar seekBar;
+    private Timer timer;
+    private int max =0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_media_internet_video);
         mediaInternetVideoEdit = (EditText) findViewById(R.id.mediaInternetVideoEdit);
 
-
-
+        seekBar = (SeekBar) findViewById(R.id.mediaInternetVideoSeekBar);
         button = (Button) findViewById(R.id.mediaInternetVideoBtn1);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -55,6 +61,26 @@ public class ActivityMediaInternetVideo extends AppCompatActivity implements Med
                             mediaPlay.prepareAsync();
                             mediaPlay.seekTo(0);
                             aBoolean =true;
+                            max = mediaPlay.getDuration();
+                            seekBar.setMax(100);
+                            Log.i("media长度","--------------"+100);
+                            timer = new Timer();
+                            TimerTask timerTask = new TimerTask() {
+                                @Override
+                                public void run() {
+//                                    if(aBoolean){
+//                                        return;
+//                                    }
+                                    if(mediaPlay.getDuration()==0||mediaPlay.getCurrentPosition()==0){
+
+                                    }else {
+                                        seekBar.setProgress((int)(100*(((float)mediaPlay.getCurrentPosition())/mediaPlay.getDuration())));
+                                        Log.i("media当前进度","--------------"+100*(((float)mediaPlay.getCurrentPosition())/mediaPlay.getDuration()));
+                                    }
+
+                                }
+                            };
+                            timer.schedule(timerTask,0,1000);
                         } catch (IOException e) {
                             e.printStackTrace();
                         }
@@ -69,6 +95,7 @@ public class ActivityMediaInternetVideo extends AppCompatActivity implements Med
 
         mediaPlay = new MediaPlayer();
         mediaPlay.setOnPreparedListener(this);
+        mediaPlay.setOnBufferingUpdateListener(this);
     }
     @Override
     public void onPrepared(MediaPlayer mp) {
@@ -78,6 +105,15 @@ public class ActivityMediaInternetVideo extends AppCompatActivity implements Med
     @Override
     protected void onDestroy() {
         mediaPlay.release();
+        if(timer!=null){
+            timer.cancel();
+        }
         super.onDestroy();
+    }
+
+    @Override
+    public void onBufferingUpdate(MediaPlayer mp, int percent) {
+        seekBar.setSecondaryProgress(percent);
+        Log.e("音乐缓存进度","-------------------------"+percent);
     }
 }
