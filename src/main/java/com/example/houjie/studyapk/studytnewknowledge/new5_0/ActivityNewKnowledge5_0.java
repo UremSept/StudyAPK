928bcb48e90b45e9a420883137ad78f626439f34
UremSept <397055873@qq.com>
2016-06-04 19:19:41 +0800

    学习阶段完成

--- src/main/java/com/example/houjie/studyapk/studytnewknowledge/new5_0/ActivityNewKnowledge5_0.java
+++ src/main/java/com/example/houjie/studyapk/studytnewknowledge/new5_0/ActivityNewKnowledge5_0.java
@@ -1,15 +1,188 @@
 package com.example.houjie.studyapk.studytnewknowledge.new5_0;
 
+import android.os.Build;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatDelegate;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.example.houjie.studyapk.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ActivityNewKnowledge5_0 extends AppCompatActivity {
 //所要隐藏的view必须要在可滚动view之前
+    private DrawerLayout mDrawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_knowledge5_0);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);//设置新的bar代替原来的bar
+        final ActionBar ab = getSupportActionBar();//获得actionBar
+        ab.setHomeAsUpIndicator(R.mipmap.ic_menu);//设置返回键位置的图标
+        ab.setDisplayHomeAsUpEnabled(true);//设置返回键是否显示
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.newKnowledgeDrawerLayout);
+        /**
+         * <!--DrawerLayout是ViewGroup的子类。
+         * 作为一个界面的顶层容器。
+         * 允许抽屉窗口从左右边缘拖出。
+         * DrawerLayout中抽屉的位置由layout_gravity控制，start代表右边end代表左边（国内）。
+         * 抽屉窗口的高度一般设置为match_parent。-->
+         */
+        NavigationView navigationView = (NavigationView) findViewById(R.id.newKnowledgeViewNavigation);
+        if (navigationView != null) {
+            setupDrawerContent(navigationView);
+        }
+        /**
+         * <!--Navigation指定抽屉的样式-->
+         */
+
+
+        //指定viewpager
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        if (viewPager != null) {
+            setupViewPager(viewPager);
+        }
+
+        //设置悬浮按钮点击事件
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
+                        .setAction("Action", new View.OnClickListener(){
+                            @Override
+                            public void onClick(View v) {
+
+                            }
+                        }).show();
+            }
+        });
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.sample_actions, menu);
+        return true;
+    }
+
+    //设置打开menu后默认选择的menu选项
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        switch (AppCompatDelegate.getDefaultNightMode()) {
+            case AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM:
+                menu.findItem(R.id.menu_night_mode_system).setChecked(true);
+                break;
+            case AppCompatDelegate.MODE_NIGHT_AUTO:
+                menu.findItem(R.id.menu_night_mode_auto).setChecked(true);
+                break;
+            case AppCompatDelegate.MODE_NIGHT_YES:
+                menu.findItem(R.id.menu_night_mode_night).setChecked(true);
+                break;
+            case AppCompatDelegate.MODE_NIGHT_NO:
+                menu.findItem(R.id.menu_night_mode_day).setChecked(true);
+                break;
+        }
+        return true;
+    }
+    //重写MenuItem选择事件
+    //返回键属于Menu
+    //Menu设置activity主题
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+            case R.id.menu_night_mode_system:
+                setNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+                break;
+            case R.id.menu_night_mode_day:
+                setNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                break;
+            case R.id.menu_night_mode_night:
+                setNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                break;
+            case R.id.menu_night_mode_auto:
+                setNightMode(AppCompatDelegate.MODE_NIGHT_AUTO);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setNightMode(int nightMode) {
+        AppCompatDelegate.setDefaultNightMode(nightMode);
+        //如果sdk版本大于11.则重启activity 可能会丢失数据
+        if (Build.VERSION.SDK_INT >= 11) {
+            recreate();
+        }
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        Adapter adapter = new Adapter(getSupportFragmentManager());//关联ViewPager与Fragment
+        adapter.addFragment(new CheeseListFragment(), "Category 1");
+        adapter.addFragment(new CheeseListFragment(), "Category 2");
+        adapter.addFragment(new CheeseListFragment(), "Category 3");
+        viewPager.setAdapter(adapter);
+    }
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        //设置某个选项被选中，并且关闭
+                        menuItem.setChecked(true);
+                        mDrawerLayout.closeDrawers();
+                        return true;
+                    }
+                });
+    }
+    //自定义FragmentPager
+    static class Adapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragments = new ArrayList<>();
+        private final List<String> mFragmentTitles = new ArrayList<>();
+
+        public Adapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragments.add(fragment);
+            mFragmentTitles.add(title);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragments.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitles.get(position);
+        }
     }
 }
---
+++ src/main/java/com/example/houjie/studyapk/studytnewknowledge/new6_0/ActivityNewKnowledge6_0.java
@@ -0,0 +1,15 @@
+package com.example.houjie.studyapk.studytnewknowledge.new6_0;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.houjie.studyapk.R;
+
+public class ActivityNewKnowledge6_0 extends AppCompatActivity {
+    //android 6.0主要变化的是动态权限的申请
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activity_new_knowledge6_0);
+    }
+}
